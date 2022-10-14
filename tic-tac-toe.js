c0ddcabee1e1840a616ba198600d44cc45bc1c62
RohenaBlack <rohena889@gmail.com>
2022-10-14 12:24:58 -0500

    Added code to allow X or O to be added to square when clicked

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -6,12 +6,31 @@ window.onload = event => {
     tileLayout();
 
     function tileLayout() {
+        arr = new Array(9);
+        lastPlayer = "O";
+
         for (let shape of tiles) {
-            shape.classList.add("square"); // Adds squares to board     
+            shape.classList.add("square"); // Adds squares to board
+            
+            shape.onclick = (event) => {
+    
+                if (lastPlayer === "X") {
+                    event.target.innerText = "O";
+                    event.target.classList.add("O");
+                    arr[event.target.id] = "O";
+                    lastPlayer = "O"; //Adds O to square if lp=X
+                }
+                else if (lastPlayer === "O") {
+                    event.target.innerText = "X";
+                    event.target.classList.add("X");
+                    arr[event.target.id] = "X";
+                    lastPlayer = "X"; //Adds X to square if lp=O
+                }
+            }; //End of shape.onlick=(event)
              
-        }
-    }
-}
+        } //End of for loop-(let shape of tiles)
+    }//End of function tileLayout
+} //End of code
  
 
 
